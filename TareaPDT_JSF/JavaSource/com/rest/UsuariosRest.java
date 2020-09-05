774e3e2abef135136e45ae657606585fd1b79f00
Unknown <unknown@example.com>
2020-09-05 20:20:57 -0300

    ABM REST

--- TareaPDT_JSF/JavaSource/com/rest/UsuariosRest.java
+++ TareaPDT_JSF/JavaSource/com/rest/UsuariosRest.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -14,7 +17,7 @@ import javax.ws.rs.core.Response;
 import com.Remote.UsuarioBeanRemote;
 import com.entidades.Usuario;
 
-@Path("/usuarios")
+@Path("usuarios")
 public class UsuariosRest {
 	
 	@EJB
