4c36dfce8cd4e2880029b7e08b6ae3d43bf73cc2
Damian <d-bozz@hotmail.com>
2020-09-15 08:43:11 -0300

    Fix listado, busca de forma independiente por los campos o con la combinación

--- TareaPDT_JSF/JavaSource/com/bean/ObservacionBean.java
+++ TareaPDT_JSF/JavaSource/com/bean/ObservacionBean.java
@@ -408,8 +408,9 @@ public class ObservacionBean implements Serializable{
 				List <Observacion> filtradas = new ArrayList<Observacion>();
 				for (Observacion o : observacionesFiltradas)
 				{
-					if (o.getLocalidad().getDepartamento().getZona().getNombre_zona().equals(zona) 
-							&& (o.getFecha().before(hasta) && o.getFecha().after(desde)))
+					if ((zona ==null || zona.isEmpty() || o.getLocalidad().getDepartamento().getZona().getNombre_zona().equals(zona))  
+							&& (hasta == null || hasta.toString().isEmpty() || o.getFecha().before(hasta)) 
+							&& (desde == null || desde.toString().isEmpty() || o.getFecha().after(desde)))
 							{
 								filtradas.add(o);
 							}
